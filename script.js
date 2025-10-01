7b58e6bbda4d59747d4bd4302761d7b4b2b4c1f9
JubesMaci <jubsmaci@gmail.com>
2025-10-01 18:32:26 -0300

    feat: creating a new expense object

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ function formatCurrencyBRL(value) {
 form.onsubmit = (event) => {
   event.preventDefault();
 };
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+  console.log(newExpense);
+};
