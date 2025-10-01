49e4cfdba9759d2cfe720233da02160af7c5dec1
JubesMaci <jubsmaci@gmail.com>
2025-10-01 19:37:04 -0300

    feat: displays the number of items in the list

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 const expenseList = document.querySelector("ul");
+const expensesQuantity = document.querySelector("aside header p span");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -70,7 +71,20 @@ function expenseAdd(newExpense) {
 
     expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
     expenseList.append(expenseItem);
+
+    updateTotals();
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
   }
+}
+
+//atualiza os totais
+function updateTotals() {
+  try {
+    const items = expenseList.children;
+
+    expensesQuantity.textContent = `${items.length} ${
+      items.length > 1 ? "despesas" : "despesa"
+    }`;
+  } catch (error) {}
 }
